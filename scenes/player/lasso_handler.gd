9297d8c26963f9ac004069045a3dc70785683757
James Minor <james.minor.business@gmail.com>
2026-03-14 05:51:05 -0500

    feat: add max lasso length

--- scenes/player/lasso_handler.gd
+++ scenes/player/lasso_handler.gd
@@ -1,22 +1,30 @@
 extends Node2D
 
+## The maximum distance the player can lasso to / from.
 @export
-var lasso_speed: float = 500
+var max_lasso_length: float
 
+## How fast the lasso pulls an object / the player.
+@export
+var lasso_speed: float
+
+## Reference to the player owner object.
 @onready
-var player: Player = owner as Player
+var _player: Player = owner as Player
 
 func lasso_to_object() -> void:
 	var space_state: PhysicsDirectSpaceState2D = get_world_2d().direct_space_state
+	
+	if global_position.distance_to(get_global_mouse_position()) > max_lasso_length:
+		return
 	
 	var query = PhysicsRayQueryParameters2D.create(global_position, get_global_mouse_position(), 16)
 	var result = space_state.intersect_ray(query)
 	
 	if result:
-		print("moving!")
 		%StateMachine.set_state("lassodash")
 		
 		%LassoLine.target_position = result["position"]
-		player.velocity += player.global_position.direction_to(result["position"]) * lasso_speed
+		_player.velocity += _player.global_position.direction_to(result["position"]) * lasso_speed
 		
-		player.move_and_slide()
+		_player.move_and_slide()
