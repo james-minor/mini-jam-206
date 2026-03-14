2fdc09e0d10f9a12316eaca51355ca030a889bdc
zach-paronto <97645671+zach-paronto@users.noreply.github.com>
2026-03-14 15:50:23 -0500

    enemy pathfinding

--- scenes/enemy/scripts/states/moving.gd
+++ scenes/enemy/scripts/states/moving.gd
@@ -1,11 +1,31 @@
 extends EnemyState
+#extends CharacterBody2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+var movement_speed: float = 200.0
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready():
+	# These values need to be adjusted for the actor's speed
+	# and the navigation layout.
+	navigation_agent.path_desired_distance = 2.0
+	navigation_agent.target_desired_distance = 2.0
+	navigation_agent.debug_enabled = true
+	set_movement_target(GlobalVariables.player_position)
+
+
+
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target
+
+
+func _physics_process(_delta):
+	if navigation_agent.is_navigation_finished():
+		return
+
+	var current_agent_position: Vector2 = enemy.global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	enemy.velocity = current_agent_position.direction_to(next_path_position) * movement_speed
+	enemy.move_and_slide()
