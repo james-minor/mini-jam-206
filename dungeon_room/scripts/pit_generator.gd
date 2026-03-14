623b70a35830f62fcaa6a806e3aa23db1f031c4f
James Minor <james.minor.business@gmail.com>
2026-03-14 05:21:29 -0500

    feat: dungeon room can now have multiple tiles queued for destruction

--- dungeon_room/scripts/pit_generator.gd
+++ dungeon_room/scripts/pit_generator.gd
@@ -2,13 +2,21 @@ extends TileMapLayer
 
 ## Time between generation of pit tiles.
 @export
-var time_between_pit_creation: float = 2
+var time_between_pit_creation: float
+
+## How long a tile marked for destruction will take to turn into a pit.
+@export
+var time_for_tile_break: float
 
 # Internal timer to track if the tilemap should create a pit tile.
 var _pit_timer: float = 0.0
 
+# Queue of what tile coordinates to convert into a pit. 
+@onready
+var _destruction_queue: Array[Vector2i] = []
 
-var _target_cell_coordinates: Vector2i
+
+
 
 func _physics_process(delta: float) -> void:
 	_pit_timer += delta
@@ -28,12 +36,21 @@ func _generate_pit_tile() -> void:
 		# Checking if tile can be converted into a pit.
 		if get_cell_tile_data(cell_coords[index]).get_custom_data_by_layer_id(0) as bool:
 			set_cell(cell_coords[index], 1, Vector2i(0, 4))
-			_target_cell_coordinates = cell_coords[index]
-			%CollapseTimer.start()
+			_destruction_queue.push_back(cell_coords[index])
+			
+			var collapse_timer: Timer = Timer.new()
+			collapse_timer.autostart = true
+			collapse_timer.wait_time = time_for_tile_break
+			collapse_timer.timeout.connect(_on_collapse_timer_timeout)
+			add_child(collapse_timer)
+			
 			return
 		
 		tiles_checked += 1
 
 
 func _on_collapse_timer_timeout() -> void:
-	set_cells_terrain_connect([_target_cell_coordinates], 0, 0)
+	get_child(0).queue_free()
+	
+	if _destruction_queue.size() > 0:
+		set_cells_terrain_connect([_destruction_queue.pop_front()], 0, 0)
