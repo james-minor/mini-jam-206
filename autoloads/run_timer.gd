2fc6cd43ecb2e27c6713a4b5b063e8e4e1732c2c
James Minor <james.minor.business@gmail.com>
2026-03-15 07:36:53 -0500

    feat: add RunTimer autoload

---
+++ autoloads/run_timer.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var _time: float = 0
+
+var _is_running: bool = false
+
+func _physics_process(delta: float) -> void:
+	if _is_running:
+		_time += delta
+
+
+## Starts the run timer.
+func start() -> void:
+	_is_running = true
+
+
+## Stops the run timer.
+func stop() -> void:
+	_is_running = false
+
+
+## Returns the run timer.
+func get_time() -> float:
+	return _time
