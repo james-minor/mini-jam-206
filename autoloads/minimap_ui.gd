bd0da364aa11800831a17ad906730ee771cde9b9
zach-paronto <97645671+zach-paronto@users.noreply.github.com>
2026-03-15 21:21:22 -0500

    map tiling; testing next

---
+++ autoloads/minimap_ui.gd
@@ -0,0 +1,17 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	MapState.set_room_active(PlayerTracker.current_room)
+	var i: int = 1
+	var children = self.get_children()
+	for room in MapState.rooms:
+		if room == MapState.RoomVisibility.EMPTY or room == MapState.RoomVisibility.EMPTY:
+			continue
+		if room == MapState.RoomVisibility.REVEALED:
+			children[i].Color = MapState.COLORS[MapState.RoomVisibility.REVEALED]
+		elif room == MapState.RoomVisibility.ENTERED:
+			children[i].Color = MapState.COLORS[MapState.RoomVisibility.ENTERED]
+		else:
+			children[i].Color = MapState.COLORS[MapState.RoomVisibility.ACTIVE]
